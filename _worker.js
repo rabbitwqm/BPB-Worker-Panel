750fcd018b610a47f9bc6a2cc1eec3c78f6e91fe
rabbitwqm <rabbitwqm@gmail.com>
2024-11-10 20:39:59 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+var userID = "08f1cd2b-1b6a-4521-a3f2-c7722c77a019";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
